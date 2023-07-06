5e76c9bedaec840e4150e61c0bb0967004b02c7d
Graweed <niks@precisionlex.com>
2023-07-06 06:47:24 +0300

    Triangle half

--- src/lve_app_base.hpp
+++ src/lve_app_base.hpp
@@ -25,17 +25,16 @@ class LveAppBase {
   void run();
 
  private:
-  void createPipelineLayout();
-  void createPipeline();
-  void createCommandBuffers();
-  void drawFrame();
-
-
   LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
   LveDevice lveDevice{lveWindow};
   LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
   std::unique_ptr<LvePipeline> lvePipeline;
   VkPipelineLayout pipelineLayout;
   std::vector<VkCommandBuffer> commandBuffers;
+
+  void createPipelineLayout();
+  void createPipeline(); 
+  void createCommandBuffers();
+  void drawFrame();
 };
 }  // namespace lve
--- src/lve_pipeline.hpp
+++ src/lve_pipeline.hpp
@@ -2,52 +2,61 @@
 
 #include "lve_device.hpp"
 
+// std
 #include <string>
 #include <vector>
 
 namespace lve {
-    struct PipelineConfigInfo {
-        VkViewport viewport;
-        VkRect2D scissor;
-        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
-        VkPipelineRasterizationStateCreateInfo rasterizationInfo;
-        VkPipelineMultisampleStateCreateInfo multisampleInfo;
-        VkPipelineColorBlendAttachmentState colorBlendAttachment;
-        VkPipelineColorBlendStateCreateInfo colorBlendInfo;
-        VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
-        VkPipelineLayout pipelineLayout = nullptr;
-        VkRenderPass renderPass = nullptr;
-        uint32_t subpass = 0;
-    };
-    class LvePipeline {
-    public:
-        LvePipeline(
-            LveDevice &device, 
-            const std::string &vertFilepath, 
-            const std::string &fragFilepath,
-            const PipelineConfigInfo &configInfo
-            );
-        ~LvePipeline();
 
-        LvePipeline(const LvePipeline &) = delete;
-        void operator=(const LvePipeline &) = delete;
+struct PipelineConfigInfo {
+  PipelineConfigInfo() = default;
+  PipelineConfigInfo(const PipelineConfigInfo&) = delete;
+  PipelineConfigInfo& operator=(const PipelineConfigInfo&) = delete;
 
-        static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
-    private:
-        static std::vector<char> readFile(const std::string &filepath);
-        void createGraphicsPipeline(
-            const std::string &vertFilepath, 
-            const std::string& fragFilepath, 
-            const PipelineConfigInfo &configInfo
-            );
+  VkViewport viewport;
+  VkRect2D scissor;
+  VkPipelineViewportStateCreateInfo viewportInfo;
+  VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+  VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+  VkPipelineMultisampleStateCreateInfo multisampleInfo;
+  VkPipelineColorBlendAttachmentState colorBlendAttachment;
+  VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+  VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+  VkPipelineLayout pipelineLayout = nullptr;
+  VkRenderPass renderPass = nullptr;
+  uint32_t subpass = 0;
+};
 
-        void createShaderModule(const std::vector<char> &code, VkShaderModule *shaderModule);
+class LvePipeline {
+ public:
+  LvePipeline(
+      LveDevice& device,
+      const std::string& vertFilepath,
+      const std::string& fragFilepath,
+      const PipelineConfigInfo& configInfo);
+  ~LvePipeline();
 
-        LveDevice &lveDevice;
-        VkPipeline graphicsPipeline;
-        VkShaderModule vertShaderModule;
-        VkShaderModule fragShaderModule;
-    };
-    
-} // namespace lve
+  LvePipeline(const LvePipeline&) = delete;
+  void operator=(const LvePipeline&) = delete;
 
+  void bind(VkCommandBuffer commandBuffer);
+
+  static void defaultPipelineConfigInfo(
+      PipelineConfigInfo& configInfo, uint32_t width, uint32_t height);
+
+ private:
+  static std::vector<char> readFile(const std::string& filepath);
+
+  void createGraphicsPipeline(
+      const std::string& vertFilepath,
+      const std::string& fragFilepath,
+      const PipelineConfigInfo& configInfo);
+
+  void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
+
+  LveDevice& lveDevice;
+  VkPipeline graphicsPipeline;
+  VkShaderModule vertShaderModule;
+  VkShaderModule fragShaderModule;
+};
+}  // namespace lve
